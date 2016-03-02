b7012d98039dda283e759165a5a575d536294af6
David Scales <davidsscales@gmail.com>
2016-03-02 08:46:25 -0800

    Improve grunt documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,35 @@
 /*
   Code Partly From Udacity Responsive Images Course
 
-  "grunt" alone creates a new, completed images directory
-  "grunt clean" removes the images directory
-  "grunt responsive_images" re-processes images without removing the old ones
+  Automatically resize and optimize images, minify and internalize css,
+  minify html and inline js, and manage the associate files and directories.
 */
 
 module.exports = function(grunt) {
   grunt.initConfig({
-    /* Minify html https://github.com/gruntjs/grunt-contrib-htmlmin */
+
+    /* Minify my main html file, index.html
+    https://github.com/gruntjs/grunt-contrib-htmlmin */
     htmlmin: {
       dist: {
         options: {
-          removeComments: true,
-          removeCommentsFromCDATA: true,
-          removeScriptTypeAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          minifyJS: true,
-          collapseWhitespace: true,
-          conservativeCollapse: true
+          removeComments: true, // remove comments from html
+          removeCommentsFromCDATA: true, // remove comments from <script> and <style>
+          removeScriptTypeAttributes: true, // remove unnecessary <script> attributes
+          removeStyleLinkTypeAttributes: true, // remove unnecessary <style> attributes
+          minifyJS: true, // minify inline js
+          collapseWhitespace: true, // remove whitespace
+          conservativeCollapse: true // preserve a single whitespace, to prevent potential errors
         },
         files: {
-          'index.min.html': 'index.html'
+          'index.min.html': 'index.html' // destination : source
         }
       }
     },
 
-    /* Inline and minify css https://github.com/chyingp/grunt-inline */
+    /*  Minify and internalize style.css into <style> in index.html
+     * Mark href with ?__inline=true
+     * https://github.com/chyingp/grunt-inline */
     inline: {
       dist: {
         options: {
@@ -37,10 +40,12 @@ module.exports = function(grunt) {
       }
     },
 
-    /* run after responsive_images for further optimizations with ImageOptim.
-    Leave jPeg mini false, its not installed (its $20!)
-    You can set imageAlpha to true, it works on png's only and is lossy
-    ImageOptim is already true (all are true by default) */
+    /* Optimize images with ImageOptim
+    Run after responsive_images for further optimizations.
+    Leave jPeg mini false, its not installed (its $20!).
+    You can set imageAlpha to true, it works on png's only and is lossy.
+    ImageOptim is already true (all are true by default)
+    https://github.com/JamieMason/grunt-imageoptim */
     imageoptim: {
       myTask: {
         options: {
@@ -51,11 +56,12 @@ module.exports = function(grunt) {
       }
     },
 
-    /* resize and optimize images */
+    /* Resize and optimize images with ImageMagick
+    https://github.com/andismith/grunt-responsive-images */
     responsive_images: {
       dev: {
         options: {
-          engine: 'im',
+          engine: 'im', // engine is for ImageMagick command line tool
           sizes: [{
             width: 115,
             rename: false,
